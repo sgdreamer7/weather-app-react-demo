5cafb0a74138ffbe68d1403a0f6019944cc5d167
sgdreamer7 <vns.scherbina@gmail.com>
2018-02-06 16:48:16 +0200

    Change class name to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Navbar } from 'reactstrap'
 
-export default class Header extends React.Component {
+export default class Footer extends React.Component {
   render() {
     return (
       <footer>
         <Navbar color='dark' dark expand='md' fixed='bottom'>
           <span className='text-muted ml-auto'>Vladimir Scherbina (C) {new Date().getUTCFullYear()}, <a href='mailto:vns.scherbina@gmail.com'>vns.scherbina@gmail.com</a></span>
-          </Navbar>
+        </Navbar>
       </footer>
     )
   }
